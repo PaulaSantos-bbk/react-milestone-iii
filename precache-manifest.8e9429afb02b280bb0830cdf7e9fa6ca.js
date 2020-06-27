d91f1529553ebd0c347f9222cee4013dc78b6609
unknown <pangeriz@outlook.com>
2020-06-27 16:01:38 +0100

    Updates

---
+++ precache-manifest.8e9429afb02b280bb0830cdf7e9fa6ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4ec73ac1be0ee4ffdda24bd755ee542",
+    "url": "/react-milestone-iii/index.html"
+  },
+  {
+    "revision": "1ebcf8cee2de05e0c3ad",
+    "url": "/react-milestone-iii/static/css/main.9b59f428.chunk.css"
+  },
+  {
+    "revision": "c1b0d5d36d1ebcc0cc91",
+    "url": "/react-milestone-iii/static/js/2.19914624.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-milestone-iii/static/js/2.19914624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ebcf8cee2de05e0c3ad",
+    "url": "/react-milestone-iii/static/js/main.a0885e82.chunk.js"
+  },
+  {
+    "revision": "1531ac3193a30354f713",
+    "url": "/react-milestone-iii/static/js/runtime-main.2e9de6c0.js"
+  }
+]);
